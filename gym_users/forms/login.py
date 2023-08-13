c2022ef1ba8046eebcf44375e29cc8a2b78691a4
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-14 00:57:35 +0500

    Update login.py

--- gym_users/forms/login.py
+++ gym_users/forms/login.py
@@ -3,10 +3,9 @@ from gym_users.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class UserLoginForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['username', 'password']
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
 
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
@@ -14,12 +13,12 @@ class UserLoginForm(forms.ModelForm):
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password'].widget.attrs['class'] = 'form-control'
 
-    # def clean_username(self):
-    #     email = self.cleaned_data.get('username')
-    #     exists= User.objects.filter(email=email).first()
-    #     print(exists, 'existss')
-    #     if exists:
-    #         print('here')
-    #         return exists.email
-    #     raise forms.ValidationError("Email Doesn't Exists")
+    def validate_username(self):
+        email = self.cleaned_data.get('username')
+        exists= User.objects.filter(email=email).first()
+        print(exists, 'existss')
+        if exists:
+            print('here')
+            return exists.email
+        raise forms.ValidationError("Email Doesn't Exists")
 
