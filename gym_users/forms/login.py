e7ac673b3c6056f42737e6f5c45d0db45832f489
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-13 22:50:52 +0500

    login form

--- gym_users/forms/login.py
+++ gym_users/forms/login.py
@@ -2,7 +2,7 @@ from django import forms
 from gym_users.models import User
 
 
-class UserLoginForm(forms.Form):
+class UserLoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email', 'password']
--- gym_users/views/login.py
+++ gym_users/views/login.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
+from gym_users.forms.login import UserLoginForm
+
 
 class LoginView(View):
     def dispatch(self, request, *args, **kwargs):
         return super(LoginView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request):
-        # form = LoginForm()
-        context = {'form': ''}
+        form = UserLoginForm()
+        context = {'form': form}
         return render(request, 'gym_users/login.html', context=context)
