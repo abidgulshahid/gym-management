4511a13dc520b281aad9e8cb5b7afc84ef71a1b6
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-14 00:27:45 +0500

    Update signup.py

--- gym_users/forms/signup.py
+++ gym_users/forms/signup.py
@@ -1,11 +1,13 @@
 from django import forms
 from gym_users.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class UserSignUpForm(forms.ModelForm):
+    birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
     class Meta:
         model = User
-        fields = ['email', 'password', 'type']
+        fields = ['email', 'password', 'type', 'birth_date']
     TYPE_CHOICES = [("coach", 'COACH'), ("user", 'USER')]
 
     def __init__(self, *args, **kwargs):
@@ -14,6 +16,11 @@ class UserSignUpForm(forms.ModelForm):
         self.fields['type'] = forms.ChoiceField(choices=self.TYPE_CHOICES)
         self.fields['email'].widget.attrs['class'] = 'form-control'
         self.fields['password'].widget.attrs['class'] = 'form-control'
+        self.fields['birth_date'].widget.attrs['type'] = 'datetime'
+        self.fields['birth_date'].widget = forms.TextInput(attrs={'type': 'date'})
+        self.fields['birth_date'].widget.attrs['class'] = 'form-control'
+
+
         self.fields['type'].widget.attrs['class'] = 'form-control'
 
     # def clean_email(self):
