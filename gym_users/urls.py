31dd81b3835c77f9f9b5b4badf7f9695ec293821
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-13 19:54:32 +0500

    assets and css

--- gym_users/urls.py
+++ gym_users/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+from gym_users.views.index import IndexView
 
+urlpatterns = [
+    path('', IndexView.as_view(), name='index_view'),
+]
 
---
+++ gym_users/views/index.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class IndexView(View):
+    def dispatch(self, request, *args, **kwargs):
+        return super(IndexView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        # form = LoginForm()
+        context = {'form': ''}
+        return render(request, 'gym_users/index.html', context=context)
