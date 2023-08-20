55f0e8f7d54a12e82071e9a3be85b00e0fc9f0b9
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-21 00:48:38 +0500

    Update settings.py

--- gym_main/settings.py
+++ gym_main/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gym_users'
+    'gym_users',
+    'gym_dashboard'
 ]
 
 MIDDLEWARE = [
