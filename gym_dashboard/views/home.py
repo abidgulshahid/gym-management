8589cd10c9531902a8c2ff228e58a430fe72fa52
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-21 00:48:36 +0500

    Update home.py

--- gym_dashboard/views/home.py
+++ gym_dashboard/views/home.py
@@ -3,11 +3,12 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import View
 
-
+print('hello')
 class Dashboard(View):
     def dispatch(self, request, *args, **kwargs):
         return super(Dashboard, self).dispatch(request, *args, **kwargs)
 
     def get(self, request):
-        if request.user.is_authenticated:return render(request, 'gym_dasboard/dashboard.html', context={})
+        print(request.user)
+        if request.user.is_authenticated:return render(request, 'gym_dashboard/dashboard.html', context={})
         return HttpResponseRedirect(reverse_lazy('index_view'))
