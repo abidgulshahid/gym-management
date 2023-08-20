d112cd5b578e02d95c992e0016d181a48a17b380
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-21 00:48:34 +0500

    Update urls.py

--- gym_dashboard/urls.py
+++ gym_dashboard/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from gym_dashboard.views.home import Dashboard
 
 urlpatterns = [
-    path('/home',Dashboard.as_view(), name='dasboard_view')
+    path('',Dashboard.as_view(), name='dashboard_view')
 ]
