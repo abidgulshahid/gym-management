dd09ec295628fbf7c2d7454cf1e667decb5a3189
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-26 22:57:15 +0500

    Update urls.py

--- gym_users/urls.py
+++ gym_users/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from gym_users.views.index import IndexView
 from gym_users.views.login import LoginView
 from gym_users.views.signup import SignUpView
+from gym_users.views.logout import LogoutView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index_view'),
     path('login/', LoginView.as_view(), name='login_view'),
     path('signup/', SignUpView.as_view(), name='signup_view'),
+    path('logout/', LogoutView.as_view(), name='logout_view')
+
 ]
