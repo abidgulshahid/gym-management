ff8acc4059805869f296aeba32360f01dfbe8438
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-26 23:36:50 +0500

    Update index.py

--- gym_users/views/index.py
+++ gym_users/views/index.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
+from gym_users.models import ScheduleClass
+
 
 class IndexView(View):
     def dispatch(self, request, *args, **kwargs):
         return super(IndexView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request):
-        context = {'form': ''}
+        classes = ScheduleClass.objects.all()
+        context = {'form': '', 'classes': classes}
         return render(request, 'gym_users/index.html', context=context)
