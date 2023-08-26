32467d5e0017a4cfc58086542402df9ee19eb083
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-27 03:39:42 +0500

    Create payment.py

---
+++ gym_dashboard/forms/payment.py
@@ -0,0 +1,16 @@
+from django import forms
+from gym_users.models import User, Payments
+from django.contrib.auth.forms import UserCreationForm
+
+
+class PaymentForm(forms.ModelForm):
+    class Meta:
+        model = Payments
+        fields = ['bank_account_number', 'zip', 'Address']
+
+    def __init__(self, *args, **kwargs):
+        super(PaymentForm, self).__init__(*args, **kwargs)
+
+        self.fields['zip'].widget.attrs['class'] = 'form-control'
+        self.fields['Address'].widget.attrs['class'] = 'form-control'
+        self.fields['bank_account_number'].widget.attrs['class'] = 'form-control'
