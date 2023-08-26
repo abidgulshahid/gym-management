2574fca54f537fd48c1dd79447a2c6c921dc5e45
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-26 23:36:43 +0500

    Update manager.py

--- gym_users/manager.py
+++ gym_users/manager.py
@@ -9,7 +9,7 @@ class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Email is Required')
-        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
