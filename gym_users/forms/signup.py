0050823465432d03a6fe290bbadce0c2236600a4
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-26 22:57:09 +0500

    Update signup.py

--- gym_users/forms/signup.py
+++ gym_users/forms/signup.py
@@ -1,13 +1,16 @@
 from django import forms
 from gym_users.models import User
 from django.contrib.auth.forms import UserCreationForm
+from datetime import datetime, date
 
 
 class UserSignUpForm(forms.ModelForm):
     birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
+
     class Meta:
         model = User
         fields = ['email', 'password', 'type', 'birth_date']
+
     TYPE_CHOICES = [("coach", 'COACH'), ("user", 'USER')]
 
     def __init__(self, *args, **kwargs):
@@ -20,13 +23,21 @@ class UserSignUpForm(forms.ModelForm):
         self.fields['birth_date'].widget = forms.TextInput(attrs={'type': 'date'})
         self.fields['birth_date'].widget.attrs['class'] = 'form-control'
 
-
         self.fields['type'].widget.attrs['class'] = 'form-control'
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
         print(email)
-        exists= User.objects.filter(email=email).exists()
+        exists = User.objects.filter(email=email).exists()
         if exists:
             raise forms.ValidationError("Email Alreayt Exists")
         return True
+
+    def clean_birth_date(self):
+        birth_date = self.cleaned_data['birth_date']
+        today = date.today()
+        age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+
+        if age < 18 and self.cleaned_data.get('type') == 'coach':
+            raise forms.ValidationError("Coaches must be 18 or older.")
+        return birth_date
