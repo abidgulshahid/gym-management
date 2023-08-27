8047dacd14eb7bc8b6d87d184572a91a0b91e343
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-27 11:00:14 +0500

    Update admin.py

--- gym_users/admin.py
+++ gym_users/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from gym_users.models import User, Payments, ScheduleClass
+from gym_users.models import User, Payments, ScheduleClass, ContactForm
 # Register your models here.
 
 
 admin.site.register(User)
 admin.site.register(ScheduleClass)
 admin.site.register(Payments)
+admin.site.register(ContactForm)
