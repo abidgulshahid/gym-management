45d716eddf8ff01495984bf106d1aaa0bf7188cc
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-27 11:13:35 +0500

    Update models.py

--- gym_users/models.py
+++ gym_users/models.py
@@ -96,3 +96,6 @@ class Payments(models.Model):
     bank_account_number = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.user)
