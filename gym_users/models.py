dde72d1e1d04620f4553ff023d9f620feefc48b2
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-27 11:09:39 +0500

    Update models.py

--- gym_users/models.py
+++ gym_users/models.py
@@ -46,10 +46,14 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
+    father_name = models.CharField(max_length=255, null=True, blank=True )
     birth_date = models.DateField(null=True, blank=True)
     is_deleted = models.BooleanField(default=False)
     modified_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.user)
 
 
 @receiver(post_save, sender=User)
