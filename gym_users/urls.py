9a3b8b6646e4af45e95d7b64280e2e1159dadc65
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-08-27 11:00:27 +0500

    Update urls.py

--- gym_users/urls.py
+++ gym_users/urls.py
@@ -3,11 +3,12 @@ from gym_users.views.index import IndexView
 from gym_users.views.login import LoginView
 from gym_users.views.signup import SignUpView
 from gym_users.views.logout import LogoutView
+from gym_users.views.contact import ContactView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index_view'),
     path('login/', LoginView.as_view(), name='login_view'),
     path('signup/', SignUpView.as_view(), name='signup_view'),
-    path('logout/', LogoutView.as_view(), name='logout_view')
-
+    path('logout/', LogoutView.as_view(), name='logout_view'),
+    path('contact/', ContactView.as_view(), name='contact_view')
 ]
