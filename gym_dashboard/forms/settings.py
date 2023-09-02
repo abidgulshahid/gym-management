63a6b69d080f4ef875440d962c9cda4e37951ba9
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-09-03 01:27:03 +0500

    Create settings.py

---
+++ gym_dashboard/forms/settings.py
@@ -0,0 +1,13 @@
+from django import forms
+from gym_users.models import User, Payments, Profile
+from django.contrib.auth.forms import UserCreationForm
+
+
+class SettingsForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        excludes = ['created_at', 'modified_at', 'is_deleted']
+
+    def __init__(self, *args, **kwargs):
+        super(SettingsForm, self).__init__(*args, **kwargs)
