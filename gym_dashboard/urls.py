23a8531b880987ae14e4b57a8a9523f16b9fcf87
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-09-03 01:27:10 +0500

    Update urls.py

--- gym_dashboard/urls.py
+++ gym_dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from gym_dashboard.views.home import Dashboard
+from gym_dashboard.views.settings import SettingView
 
 urlpatterns = [
-    path('',Dashboard.as_view(), name='dashboard_view')
+    path('',Dashboard.as_view(), name='dashboard_view'),
+    path('settings/',SettingView.as_view(), name='settings_view')
 ]
