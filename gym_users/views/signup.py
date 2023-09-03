4ed51f72a45a1e8d728ea6a063e9c9c179257ceb
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-09-04 02:14:53 +0500

    signup

--- gym_users/views/signup.py
+++ gym_users/views/signup.py
@@ -41,6 +41,12 @@ class SignUpView(View):
             user.refresh_from_db()
             user.profile.father_name = data['father_name']
             user.profile.birth_date = data['birth_date']
+            user.profile.cnic = data['cnic']
+            user.profile.address = data['address']
+            user.profile.experience = data['experience']
+            user.profile.gym_time = data['gym_time']
+            user.profile.memebership_date = data['memebership_date']
+            user.profile.mobile_no = data['mobile_no']
             user.save()
             auth = authenticate(**data)
             if auth:
