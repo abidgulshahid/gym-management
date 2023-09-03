70e6d3ffa39ac5e7d16b042b90e358a8467092b2
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-09-04 01:58:18 +0500

    Update models.py

--- gym_users/models.py
+++ gym_users/models.py
@@ -111,5 +111,11 @@ class Payments(models.Model):
 
 
 class Equipment(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField( null=True, blank=True, default='h')
+    delivery_date= models.DateTimeField(default=datetime.datetime.now())
+    muscles_used = models.CharField(max_length=255, null=True, blank=True)
+    cost = models.BigIntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
