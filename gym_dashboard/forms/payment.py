80390d22149316abd28f9d489a9afce576349b74
Abid Gul Shahid <abidgulshahid@gmail.com>
2023-09-06 12:04:32 +0500

    Update payment.py

--- gym_dashboard/forms/payment.py
+++ gym_dashboard/forms/payment.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payments
-        fields = ['bank_account_number', 'zip', 'Address', 'amount']
+        fields = ['bank_account_name','bank_account_number', 'zip', 'Address', 'amount']
 
     def __init__(self, *args, **kwargs):
         super(PaymentForm, self).__init__(*args, **kwargs)
@@ -14,4 +14,5 @@ class PaymentForm(forms.ModelForm):
         self.fields['zip'].widget.attrs['class'] = 'form-control'
         self.fields['Address'].widget.attrs['class'] = 'form-control'
         self.fields['bank_account_number'].widget.attrs['class'] = 'form-control'
+        self.fields['bank_account_name'].widget.attrs['class'] = 'form-control'
         self.fields['amount'].widget.attrs['class'] = 'form-control'
